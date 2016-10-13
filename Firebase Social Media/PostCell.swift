2a00b5b3f8364e5f90f82f00ef1b028da28de5f2
Wfuger <wfuger@gmail.com>
2016-10-13 16:10:20 -0500

    feed-ui version 2

---
+++ Firebase Social Media/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Firebase Social Media
+//
+//  Created by Will Fuger on 10/13/16.
+//  Copyright © 2016 BoogieSquad. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
