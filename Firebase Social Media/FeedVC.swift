add21a27aa2d6c2e3ea50df9472777bb43b7c3b7
Wfuger <wfuger@gmail.com>
2016-10-13 17:42:59 -0500

    Get posts from firebase

--- Firebase Social Media/FeedVC.swift
+++ Firebase Social Media/FeedVC.swift
@@ -20,6 +20,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            if let value = snapshot.value {
+                print("WILL: Posts from firebase  \(value)")
+            } else {
+                print("WILL: Error in db acccess")
+            }
+        })
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -38,7 +45,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBAction func signOutPressed(_ sender: AnyObject) {
         let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
-        print("WILL: Removed user from keychain \(keychainResult)")
+        print("WILL: Removed user id from keychain \(keychainResult)")
         try! FIRAuth.auth()?.signOut()
         self.dismiss(animated: true, completion: nil)
         
