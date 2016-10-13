3c12d81be226e08a8e7d373904386806234b129a
Wfuger <wfuger@gmail.com>
2016-10-12 22:57:31 -0500

    Oauth with Facebook and Firebase integrated

--- Firebase Social Media/SignInVC.swift
+++ Firebase Social Media/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,31 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnPressed(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if let error = error {
+                print("WILL: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("WILL: User cancelled Facebook authentication")
+            } else {
+                print("WILL: Successfully authenticated with Facebook\n")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if let error = error {
+                print("WILL: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("WILL: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
